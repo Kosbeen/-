37d7bd14af321c9a527933eb6c73d8ea092bca55
Kosbeen <kosbeen44@gmail.com>
2021-08-27 12:34:09 +0300

    добавил общие карточки, буду думать как добавлять по одной, затем удалять ту, которую выбрали

--- src/js/page/film.page.js
+++ src/js/page/film.page.js
@@ -1,5 +1,4 @@
 
-
 import films from "../services/film.service";
 
 const buttonAdd = document.querySelector('.film-cards-button-add')
@@ -13,35 +12,30 @@ function showFilms() {
     const filmsShow = films.getFilms().then(res => createTEmplate(res));
 }
 
-// function filmReduce(res) {
-// const reduceFilms = res.reduce((acc,el,index) => {
-//     acc[index] = el
-//     return acc
-// },{});
-
-// const {0: {title}} = reduceFilms
-
-// console.log(title  )
-// const template = document.querySelector('.film-cards-container');
-// const templateElemnt = cardTemplate(reduceFilms)
-// }
-
-// function cardTemplate(reduceFilms) {
-
-// }
-
-
 function createTEmplate(res) {
+    console.log(res)
 const template = document.querySelector('.film-cards-container');
-const cardsAdd = res.forEach(el => {
-    const card = addCards(el.title)
+const cardsAdd = res.forEach((el) => {
+    const card = addCards(el)
+    template.insertAdjacentHTML('afterbegin', card)
 });
-template.insertAdjacentHTML('afterbegin', cardsAdd)
+
 }
 
 function addCards(el) {
     return `
-    <div> ${el.title}</div>
+    <div class="card-container-film id="${el.id}">
+    <h1 class="card-title-film">${el.title}</h1>
+    <ul class="card-list-film">
+    <li class="film-card-list-item-1 card-list-item-all">Producer: ${el.producer}</li>
+    <li class="film-card-list-item-2 card-list-item-all">Original title: ${el.original_title}</li>
+    <li class="film-card-list-item-3 card-list-item-all">Romanced title: ${el.original_title_romancde}</li>
+    <li class="film-card-list-item-4 card-list-item-all">Release date: ${el.release_date}</li>
+    <li class="film-card-list-item-5 card-list-item-all">Running time: ${el.running_time}</li>
+    <li class="film-card-list-item-6 card-list-item-all">Score: ${el.rt_score}</li>
+    <p class="film-card-text">Description: ${el.description}</p>
+    </ul>
+    </div>
     `
 }
 
