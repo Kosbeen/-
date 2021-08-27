aee63e1b93abe6954b68d04faccf6112f71313c7
Kosbeen <kosbeen44@gmail.com>
2021-08-27 16:46:39 +0300

    добавил немного нового функционала

--- src/js/page/film.page.js
+++ src/js/page/film.page.js
@@ -1,26 +1,39 @@
 
 import films from "../services/film.service";
 
-const buttonAdd = document.querySelector('.film-cards-button-add')
+const buttonAdd = document.querySelector('.film-cards-button-add');
+const buttonRemove = document.querySelector('.film-cards-button-remove');
 
 const buttonAddAction = buttonAdd.addEventListener('click', e => {
-    e.preventDefault
+    e.preventDefault;
     showFilms()
 })
+
 
 function showFilms() {
     const filmsShow = films.getFilms().then(res => createTEmplate(res));
 }
 
 function createTEmplate(res) {
-    console.log(res)
-const template = document.querySelector('.film-cards-container');
-const cardsAdd = res.forEach((el) => {
-    const card = addCards(el)
-    template.insertAdjacentHTML('afterbegin', card)
-});
-
+    const template = document.querySelector('.film-cards-container');
+   for (let i = 0; i < res.length; i++) {
+       const card = addCards(res[i]);
+       template.insertAdjacentHTML('afterbegin', card)
+   }
 }
+
+const filmContainer = document.querySelector('.film-cards-container');
+
+
+export const buttonAddRemove = buttonRemove.addEventListener('click', e => {
+    const filmContainerChildren = filmContainer.children;
+    const filmCard = document.querySelectorAll('.card-container-film')
+   for (let i = 0; i < filmCard.length; i++) {
+       filmCard[i].remove()
+   }
+})
+
+
 
 function addCards(el) {
     return `
@@ -29,15 +42,22 @@ function addCards(el) {
     <ul class="card-list-film">
     <li class="film-card-list-item-1 card-list-item-all">Producer: ${el.producer}</li>
     <li class="film-card-list-item-2 card-list-item-all">Original title: ${el.original_title}</li>
-    <li class="film-card-list-item-3 card-list-item-all">Romanced title: ${el.original_title_romancde}</li>
+    <li class="film-card-list-item-3 card-list-item-all">Romanced title: ${el.original_title_romanised}</li>
     <li class="film-card-list-item-4 card-list-item-all">Release date: ${el.release_date}</li>
     <li class="film-card-list-item-5 card-list-item-all">Running time: ${el.running_time}</li>
     <li class="film-card-list-item-6 card-list-item-all">Score: ${el.rt_score}</li>
     <p class="film-card-text">Description: ${el.description}</p>
+    <div class="film-comment-button-container">
+            <button class="film-comment-button">
+              <a href="#" class="film-comment-button-link">Leave comment</a>
+            </button>
+          </div>
     </ul>
     </div>
     `
 }
+
+
 
 export default buttonAddAction
 
